6f31b14b3da96cce46d132d882d2328bbac582a1
AnnaBicelli <you@example.com>
2023-12-22 16:15:31 +0100

    separeted the fucnitons

--- function.py
+++ function.py
@@ -3,4 +3,12 @@ def descending_number_list(a, b, c):
 
 def print_descending_number_list(a, b, c):
     print(descending_number_list(a,b,c))
-    
+
+def create_dictionary(list_of_keys: list[str], list_of_values: list[int], n: int, d:dict):
+    '''
+    inputs: a list of keys and a list of list_of_values
+    outputs: a dictionary with the input keys abd list_of_values
+    '''
+
+    pass
+
